84487651bca22b5bd72db7699b808b089b9f1441
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-01 12:25:06 +0500

    Разработанный пользовательский элемент собственника. Белослудцева

---
+++ пр51/Elements/Owner.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace пр51.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Owner.xaml
+    /// </summary>
+    public partial class Owner : UserControl
+    {
+        public Owner(Context.OwnerContext roomOwner)
+        {
+            InitializeComponent();
+            // Задаём значение текстовому полю
+            NameOwner.Content = $"{roomOwner.LastName} {roomOwner.FirstName} {roomOwner.SurName}";
+        }
+    }
+}
