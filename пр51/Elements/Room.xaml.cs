fccb4ca8dddca9bc75dd57fc073e1e862741385e
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-01 12:28:53 +0500

    Разработанный пользовательский элемент квартиры. Белослудцева

---
+++ пр51/Elements/Room.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using пр51.Context;
+
+namespace пр51.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Room.xaml
+    /// </summary>
+    public partial class Room : UserControl
+    {
+        // Ссылка: 1
+        public Room(int Room)
+        {
+            InitializeComponent();
+            // Указываем номер квартиры
+            NameRoom.Content = "Квартира №" + Room;
+            // Вызываем загрузку данных
+            LoadOwner(Room);
+        }
+
+        /// <summary> Загрузка данных
+        // Ссылка: 1
+        public void LoadOwner(int Room)
+        {
+            // Получаем жильцов квартиры
+            List<OwnerContext> roomOwners = OwnerContext.AllOwners().FindAll(x => x.NumberRoom == Room);
+            // Добавляем элементы в stack panel
+            foreach (OwnerContext roomOwner in roomOwners)
+            {
+                Parent.Children.Add(new Elements.Owner(roomOwner));
+            }
+        }
+    }
+}
